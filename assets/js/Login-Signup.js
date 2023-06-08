488d30694df38eca8be582391db63e0c950d8632
CreepySenpai <tuana8255@gmail.com>
2023-06-08 08:14:11 +0700

    Fixed smth

--- assets/js/Login-Signup.js
+++ assets/js/Login-Signup.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const forms = document.querySelector(".forms"),
       pwShowHide = document.querySelectorAll(".eye-icon"),
       links = document.querySelectorAll(".link");
@@ -23,30 +22,4 @@ links.forEach(link => {
         e.preventDefault();
         forms.classList.toggle("show-signup");
     })
-=======
-const forms = document.querySelector(".forms"),
-      pwShowHide = document.querySelectorAll(".eye-icon"),
-      links = document.querySelectorAll(".link");
-
-pwShowHide.forEach(eyeIcon => {
-    eyeIcon.addEventListener("click", () => {
-        let pwFields = eyeIcon.parentElement.parentElement.querySelectorAll(".password");
-        pwFields.forEach(password => {
-            if(password.type == "password"){
-                password.type = "text";
-                eyeIcon.classList.replace("bx-hide", "bx-show");
-                return;
-            }
-            password.type = "password";
-            eyeIcon.classList.replace("bx-show", "bx-hide");
-        })
-    })
-})
-
-links.forEach(link => {
-    link.addEventListener("click", e => {
-        e.preventDefault();
-        forms.classList.toggle("show-signup");
-    })
->>>>>>> 3f6c873183e4413d02aad84de49560ed7f9d82f9
 })
